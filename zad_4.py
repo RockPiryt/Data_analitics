27cf11a93ba61decfb0adc00e4808683b2c16266
RockPiryt <rockpiryt@gmail.com>
2025-12-12 21:06:07 +0100

    Add comments and printscreen

--- zad_4.py
+++ zad_4.py
@@ -2,11 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import brentq
 
-# --- 1. Definicja wielomianu p(x) ---
 def p(x):
     return x**4 - 3*x**3 + 2*x - 1
 
-# --- 2. Rysowanie wykresu ---
+# rys wykresu wielomianu
 x_plot = np.linspace(-5, 5, 1000)
 y_plot = p(x_plot)
 
@@ -21,22 +20,24 @@ plt.grid(True)
 plt.legend()
 plt.show()
 
-# --- 3. Szukanie przedziałów zmiany znaku ---
-xs = np.linspace(-5, 5, 2001)
+# Szukanie przedziałów zmiany znaku, sugerownay pzedzial -5,5
+xs = np.linspace(-5, 5, 2001) # 2001 punktoów-> 2000 małych przedziałów o dł 0.0005
 sign_change_intervals = []
 
 for i in range(len(xs) - 1):
-    x1, x2 = xs[i], xs[i+1]
-    y1, y2 = p(x1), p(x2)
+    x1, x2 = xs[i], xs[i+1]# patrzymy na każdy mały przedział   
+    y1, y2 = p(x1), p(x2) # liczymy wartości funkcji na koncach pzedziału
     
-    if y1 == 0:
+    if y1 == 0: # trafilismy dokladnie w pierwiastek
         sign_change_intervals.append((x1, x1))
-    elif y1 * y2 < 0:
+    elif y1 * y2 < 0: # różne znaki na końcach przedziału, wiec w przedziale jest pierwiatek
         sign_change_intervals.append((x1, x2))
 
 # usunięcie bliskich duplikatów
 unique_intervals = []
 for a, b in sign_change_intervals:
+    #porównuje obecnie analizowany przedział i ostatanio dodany -> jeśli różnica jest większa niż 1e-6, uznaje, że to nowy przedział → dodaje
+    #Jeśli różnica jest mniejsza, to traktuje go jako „duplikat” -> pomijam.
     if not unique_intervals or abs(a - unique_intervals[-1][0]) > 1e-6:
         unique_intervals.append((a, b))
 
@@ -44,17 +45,17 @@ print("Znalezione przedziały zmiany znaku:")
 for a, b in unique_intervals:
     print(f"[{a:.4f}, {b:.4f}]")
 
-# --- 4. Zastosowanie metody Brentq ---
+# Metoda Brentq
 roots = []
 
 for a, b in unique_intervals:
     if a == b:  # idealne trafienie w pierwiastek
         roots.append(a)
     else:
-        root = brentq(p, a, b)
+        root = brentq(p, a, b) # szukanie miejsca zerowego w konretnym przedziale, p - funkcja, p(x) - konretna liczba
         roots.append(root)
 
-# --- 5. Wyniki ---
 print("\nRzeczywiste pierwiastki wielomianu:")
 for r in roots:
     print(f"x ≈ {r:.10f}")
+ 
